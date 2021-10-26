2ece67b487e1b2294aba0c47024750e10da9f18c
Evan <evanfretheim@gmail.com>
2021-10-26 15:17:09 -0500

    projects

---
+++ Input/Input/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Pick a number");
+            string ans = Console.ReadLine();
+            int userans = Convert.ToInt32(ans);
+            Console.WriteLine(DateTime.Now.AddHours(userans));
+            Console.ReadLine();
+        }
+    }
+}
