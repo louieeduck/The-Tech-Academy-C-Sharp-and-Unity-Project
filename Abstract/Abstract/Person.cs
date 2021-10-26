acaf763317d2a03b7ebf8f19f7a6fd7527ae48e2
Evan <evanfretheim@gmail.com>
2021-10-26 17:00:41 -0500

    rearrange

---
+++ Abstract/Abstract/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public abstract class Person            
+    {
+        public string firstName;
+        public string lastName;
+
+        public abstract void sayName(string firstName, string lastName);
+
+    }
+    class Employee<thing> : Person, IQuittable
+    {
+        public int Id1;
+        public int Id2;
+        public thing Data { get; set; }
+        public List<thing> Thing { get; set; }
+        public override void sayName(string firstName, string lastName)
+        {
+            Console.WriteLine(firstName + lastName);
+        }
+
+        public bool Quit(Employee employee )
+        {
+            Console.WriteLine("I Quit");
+            if (Id1 == Id2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee();
+            
+
+            string firstName = "Sample ";
+            string lastName = "Student";
+            int Id = 1;
+
+            string firstName2 = "Different ";
+            string lastName2 = "Student";
+            int Id2 = 1;
+
+            employee.sayName(firstName, lastName);
+            employee.Quit(employee);
+
+            employee.sayName(firstName2, lastName2);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
